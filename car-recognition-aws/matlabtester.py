2a096148365260f450524f1128dbbcc70c3c405e
michalgdak <michal.gdak@gmail.com>
2018-02-04 12:45:06 +0100

    Creds added in a comment

--- car-recognition-aws/matlabtester.py
+++ car-recognition-aws/matlabtester.py
@@ -1,6 +1,11 @@
 '''
 Created on 22 sty 2018
 
+
+3D Object Representations for Fine-Grained Categorization
+Jonathan Krause, Michael Stark, Jia Deng, Li Fei-Fei
+4th IEEE Workshop on 3D Representation and Recognition, at ICCV 2013 (3dRR-13). Sydney, Australia. Dec. 8, 2013.
+
 @author: mgdak
 '''
 
